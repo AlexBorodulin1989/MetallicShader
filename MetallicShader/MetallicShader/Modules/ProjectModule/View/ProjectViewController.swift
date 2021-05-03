28f1809259b579f648ce030b414828c6b8cc26bc
Aleksandr Borodulin <>
2021-05-04 00:24:07 +0300

    Add background image

--- MetallicShader/MetallicShader/Modules/ProjectModule/View/ProjectViewController.swift
+++ MetallicShader/MetallicShader/Modules/ProjectModule/View/ProjectViewController.swift
@@ -25,16 +25,6 @@ class ProjectViewController: UIViewController {
 
         output.mtkViewDidLoad(mtkView)
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: animated)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: animated)
-    }
 }
 
 // MARK: Input
